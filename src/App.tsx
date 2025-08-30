96986d02b7313847c60a83aa9bc5771d51ef2cf4
aishaksl <aisha.koseoglu@gmail.com>
2025-08-31 00:05:08 +0700

    feat: add HangmanDrawing component with body parts rendering

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,17 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  console.log(wordToGuess)
+  const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
 
   return (
-
-    <div className='h-screen w-screen flex flex-col items-center justify-center'>
+    <div className='h-screen w-screen flex flex-col items-center justify-center space-y-6'>
       <div>Lose Win</div>
-      <HangmanDrawing />
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord />
-      <Keyboard />
+      <div className='self-stretch'>
+        <Keyboard />
+      </div>
+
     </div>
 
   )
--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,35 +1,36 @@
-const Head = (
+const head = (
     <div className="absolute h-[50px] w-[50px] border-[5px] rounded-full top-[48px] right-[-20px]" />
 )
 
-const Body = (
+const body = (
     <div className="absolute h-[100px] w-[5px] top-[95px] right-0 bg-black" />
 )
 
-const RightArm = (
+const rightArm = (
     <div className="absolute h-[5px] w-[100px] top-[150px] right-[-100px] bg-black -rotate-30 origin-bottom-left" />
 )
 
-const LeftArm = (
+const leftArm = (
     <div className="absolute h-[5px] w-[100px] top-[150px] right-[5px] bg-black rotate-30 origin-bottom-right" />
 )
 
-const RightLeg = (
+const rightLeg = (
     <div className="absolute h-[5px] w-[100px] top-[185px] right-[-95px] bg-black rotate-60 origin-bottom-left" />
 )
 
-const LeftLeg = (
+const leftLeg = (
     <div className="absolute h-[5px] w-[100px] top-[185px] right-0 bg-black -rotate-60 origin-bottom-right" />
 )
 
-export function HangmanDrawing() {
+const BodyParts = [head, body, rightArm, leftArm, rightLeg, leftLeg]
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number
+}
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
     return <div className="relative">
-        {Head}
-        {Body}
-        {RightArm}
-        {LeftArm}
-        {RightLeg}
-        {LeftLeg}
+        {BodyParts.slice(0, numberOfGuesses)}
 
         <div className="absolute h-[50px] w-[5px] bg-black top-0 right-0" />
         <div className="h-[5px] w-[200px] bg-black ml-[120px]" />
