291854cc48a28bc26f1ba2ae64a8198b9b258950
aishaksl <aisha.koseoglu@gmail.com>
2025-08-30 20:39:03 +0700

    feat: Implement HangmanWord component to display guessed letters

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,16 @@
 export function HangmanWord() {
 
+    const word = "test"
+    const guessedLetters: string[] = ["a"]
+
+    return <div className="flex text-7xl gap-4 uppercase ">
+        {word.split("").map((letter, index) => (
+            <span className="border-b-4">
+                <span className={guessedLetters.includes(letter) ? "visible text-green-500" : "invisible"}>
+                    {letter}
+                </span>
+            </span>
+        ))}
+    </div>
+
 }
