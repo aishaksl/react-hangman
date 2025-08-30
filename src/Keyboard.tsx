27d53686c30be81e4f1bd84f00f028a6c1c26397
aishaksl <aisha.koseoglu@gmail.com>
2025-08-30 23:15:53 +0700

    feat: Implement Keyboard component to display alphabet keys

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,1 +1,34 @@
-export function Keyboard() { }
+const keys: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+export function Keyboard() {
+    return <div className="grid grid-cols-10 gap-1 mx-4 md:mx-12">{keys.map((key: string) => {
+        return <button key={key} className="border-2 py-2 uppercase hover:bg-blue-400 focus:bg-blue-400 cursor-pointer">{key}</button>
+    })}</div>
+}
