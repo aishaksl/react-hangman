96138d3c7e5ec645d7a8a8c6e9d217f091e2641e
aishaksl <aisha.koseoglu@gmail.com>
2025-09-01 00:12:11 +0700

    feat: update Keyboard component to manage active and inactive letter states

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -32,9 +32,20 @@ type KeyboardProps = {
     inactiveLetters: string[]
     addGuessedLetter: (letter: string) => void
 }
+// Check if the current key is in the list of correctly active-iaktive letters
 
 export function Keyboard({ activeLetter, inactiveLetters, addGuessedLetter }: KeyboardProps) {
     return <div className="grid grid-cols-10 gap-1 mx-4 md:mx-12">{keys.map((key: string) => {
-        return <button key={key} onClick={() => addGuessedLetter(key)} className="border-2 py-2 uppercase hover:bg-blue-400 focus:bg-blue-400 cursor-pointer">{key}</button>
+
+        const isActive = activeLetter.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+
+        return <button key={key} onClick={() => addGuessedLetter(key)} className={`border-2 py-2 uppercase cursor-pointer
+              hover:bg-blue-400 focus:bg-blue-400
+              ${isActive ? "bg-blue-400 text-white" : ""}
+              ${isInactive ? "bg-gray-400 text-white cursor-not-allowed" : ""}`}
+            disabled={isActive || isInactive}
+        >
+            {key}</button>
     })}</div>
 }
